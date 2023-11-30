ac4c0b999c44ba33ae3671f94feae2f1f082cc21
Julian <michaeljulian50@gmail.com>
2023-11-29 20:15:19 -0500

    tarea 1

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,92 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+
+   if (x === y){
+      return true;
+   } 
+   // else {
+   //    return false;
+   // }
+
+   return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1.length === str2.length){
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+
+   // if (num < 90){
+   //    return true;
+   // } else {
+   //    return false;
+   // }
+
+   // esto se llama operadores ternarios
+   // return num < 90 ? true : false
+
+   return num < 90;
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+
+   // opcion 1
+   // if(num > 50 ){
+   //    return true;
+   // }
+   // else{
+   //    return false;
+   // }
+
+   // opcion 2
+   // return num > 50;
+
+   // opcion 3
+   return num > 50 ? true : false;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+
+   // if (num % 2 === 0){
+   //    return true;
+   // }else {
+   //    return false;
+   // }
+
+   // opcion 2
+   return num % 2 === 0 ? true : false;
+
+   // opcion 3
+   // return num % 2 === 0;
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 !== 0){
+      return true;
+   } else {
+      return false;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- javaScritp1.js
+++ javaScritp1.js
@@ -282,3 +282,4 @@ console.log(Math.max(1,2,3,4,5,6,7,8,9,10));
 console.log(Math.min(1,2,3,4,5,6,7,8,9,10));
 
 
+
